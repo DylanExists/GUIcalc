13258076ccebcaec3cd0283ce7f63fd0232e4617
DylanExists <58371510+DylanExists@users.noreply.github.com>
2020-08-14 12:31:29 -0400

    organized main.py
    organized main.py with comments

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from tkinter import *
 root = Tk()
-x = 0000000000000000000000000000000000000
+
+# setup variable section
+x = 0000000000000000000000000000000000000 # x = output box variable
 x = str(x)[:37]    # maximum characters ^
-root.title("GUIcalc")
-root.iconphoto(False, PhotoImage(file="windowicon.png"))
-frame = Frame(root, width=250, height=125).pack()
-root.resizable(0, 0)
+# setup variable section
+
+# setup window section
+root.title("GUIcalc") # window title
+root.iconphoto(False, PhotoImage(file="windowicon.png")) # window icon
+frame = Frame(root, width=250, height=125).pack() # window frame size
+root.resizable(0, 0) # window resizability (false)
+# setup window section
+
+# label section
 label1 = Label(root, text=str(x), width=32, borderwidth=5, relief="sunken", anchor=E)
-label1.place(x=8, y=10)
+label1.place(x=8, y=10) # label1 = output box
+# label section
+
 root.mainloop()
