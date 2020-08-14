a55f9320b0a54ea8c45723b95d984e97efa45e07
DylanExists <58371510+DylanExists@users.noreply.github.com>
2020-08-13 23:41:48 -0400

    updated main.py
    added number output

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from tkinter import *
 root = Tk()
+x = 0000000000000000000000000000000000000
+x = str(x)[:37]    # maximum characters ^
 root.title("GUIcalc")
 root.iconphoto(False, PhotoImage(file="windowicon.png"))
 frame = Frame(root, width=250, height=125).pack()
 root.resizable(0, 0)
+label1 = Label(root, text=str(x), width=32, borderwidth=5, relief="sunken", anchor=E)
+label1.place(x=8, y=10)
 root.mainloop()
